b09e17f5a0d38c93ecf21ba951652fb609ec3684
4lk4tr43@gmail.com <4lk4tr43@gmail.com>
2016-04-20 07:54:39 +0200

    Tracking for SiteCat

---
+++ Tracking/Sica.js
@@ -0,0 +1,37 @@
+
+var ttCampaignName="Test";
+
+function ttTrackStageClickSica(sVar, campaignName) {
+    var overrides = {};
+
+    sVar.linkTrackVars="eVar11,eVar14,eVar47,pageName,events";
+    sVar.linkTrackEvents="event18";
+
+    overrides.pageName=sVar.pageName;
+    overrides.events="event18";
+    overrides.eVar11="AdTarget|StageClick|${campaign.name}|"+campaignName;
+    overrides.eVar14=sVar.eVar14;
+    overrides.eVar47=sVar.eVar47;
+    sVar.tl(true, "o", "AdTarget|StageClick", overrides);
+}
+
+function ttTrackStageViewSica(sVar, campaignName) {
+    var overrides = {};
+
+    sVar.linkTrackVars="eVar11,eVar14,eVar47,pageName,events";
+    sVar.linkTrackEvents="event17";
+
+    overrides.pageName=sVar.pageName;
+    overrides.events="event17";
+    if (sVar11.indexOf("StageClick") > -1)
+        overrides.eVar11="AdTarget|StageView|${campaign.name}|"+campaignName;
+    overrides.eVar14=sVar.eVar14;
+    overrides.eVar47=sVar.eVar47;
+    sVar.tl(true, "o", "AdTarget|StageView", overrides);
+}
+
+// mboxDefine rauslöschen
+// Snippet zu oberst reinkopieren
+// ttCampaignName setzen
+// erstze mboxUpdate('AD_Stage_Click') -> ttTrackStageClickSica(s_one, ttCampaignName);
+// nach ttStart(); am Schluss -> ttTrackStageViewSica(s_one, ttCampaignName);
